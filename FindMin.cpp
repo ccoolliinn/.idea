28e9b0b39910c31bf3d7cc5c8a953d114a8d73b4
colin <749849251@qq.com>
2018-05-10 20:57:56 +0800

    changshi

---
+++ FindMin.cpp
@@ -0,0 +1,43 @@
+#include<vector>
+#include<math.h>
+#include<algorithm>
+using namespace std;
+class Solution
+{
+public:
+	//////可以采用二分法////////默认数组是无重复元素的//////
+	int findMin(vector<int> &nums)
+	{
+		int size = nums.size();
+		if (size == 0)
+			return 0;
+		else if(size==1)
+		{
+			return nums[0];
+		}
+		else if (size == 2)
+		{
+			return min(nums[0], nums[1]);
+		}
+		
+			int start = 0;
+			int end = nums.size() - 1;
+			while (start<end-1)
+			{
+				if (nums[start] < nums[end])
+					return nums[start];
+				int mid = start + (start + end) / 2;
+				if (nums[mid] > nums[start])
+					start = mid;
+				else if (nums[mid]<nums[start])
+				{
+					end = mid;
+				}
+
+			}
+
+		
+		return min(nums[start], nums[end]);
+
+				}
+};
